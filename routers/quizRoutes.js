82842acb79ca5efe551f64710533119a75f7f3a2
Lucky Chelani <luckychelani28@gmail.com>
2025-01-25 18:24:20 +0530

    auth

--- routers/quizRoutes.js
+++ routers/quizRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const { createQuiz, attendQuiz, fetchAllQuizzes } = require('../controllers/quizController');
 const { isAdmin } = require('../middlewares/isadmin');
+const { identifier } = require('../middlewares/identification');
 
 
 const router = express.Router();
 
 router.post('/create', isAdmin, createQuiz);
-router.post('/attend/:quizId', attendQuiz);
-router.get('/',  fetchAllQuizzes);
+router.post('/attend/:quizId',identifier, attendQuiz);
+router.get('/', identifier, fetchAllQuizzes);
 
 module.exports = router;
